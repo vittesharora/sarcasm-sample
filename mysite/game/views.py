aa332f9a74fe6179c61bc8f29867d69996509b3e
vittesharora <avittesh@gmaill.com>
2019-05-26 16:34:28 +0530

    Vittesh User_Access given

--- mysite/game/views.py
+++ mysite/game/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from .forms import SignUpForm
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def gaming(request):
+	questions=Question.objects.all()
+	return render(request, 'game/home.html', {'questions':questions})
+def detail(request, pk):
+	question=get_object_or_404(Question, pk=pk)
+	return render(request, 'game/detail.html',{'pk':pk,'question':question})
 def register(request):
 	if request.method == 'POST':
 		form = SignUpForm(request.POST)
@@ -13,17 +18,6 @@ def register(request):
 			return redirect('/game')
 	else:
 		form= SignUpForm()
-
-@login_required
-def gaming(request):
-	if request.user.is_authenticated:
-		questions=Question.objects.all()
-		return render(request, 'game/home.html', {'questions':questions})
-	else:
-		return render(request,'game/home.html',{})	
-def detail(request, pk):
-	question=get_object_or_404(Question, pk=pk)
-	return render(request, 'game/detail.html',{'pk':pk,'question':question})
 
 	return render(request, 'game/register.html', {'form':form})				 
 def opening(request):
